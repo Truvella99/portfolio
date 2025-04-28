c3d71409167443c85dd53e94de5adfc11872a62a
Truvella99 <gagliardo9975@gmail.com>
2025-04-28 16:05:49 +0200

    fix navbar + rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable the rule for unused variables and imports
+      "@typescript-eslint/no-unused-vars": "off",
+
+      // Relax the rule for using 'any' type (set to warn instead of error)
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,7 +24,7 @@ export default function NavBar(props: any) {
     <Disclosure as="nav" className="bg-gray-800 sticky top-0 z-50">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
@@ -34,7 +34,7 @@ export default function NavBar(props: any) {
             </DisclosureButton>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch">
-            <div className="hidden md:ml-6 md:block">
+            <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
                   <a
@@ -63,7 +63,7 @@ export default function NavBar(props: any) {
         </div>
       </div>
 
-      <DisclosurePanel className="md:hidden">
+      <DisclosurePanel className="sm:hidden">
         <div className="flex flex-col items-center space-y-1 px-2 pt-2 pb-3 h-screen">
           <Image
             className="rounded-full"
